c9abf1b76dea94e9cc56bcce7252ce8125967020
RohanGautam <rohan1gautam@gmail.com>
2019-11-23 15:10:08 +0800

    added highlight radius setting property

--- highlight.js
+++ highlight.js
@@ -7,6 +7,8 @@ Run `example.html` to see a bare-bones action
 
 document.onmousemove = mhHandleMouseMove;
 
+var circleRadius = 50; // adjust this to set radius of region
+var circleDiameter = circleRadius*2;
 /* Draw a div */
 var mhNewPointer = document.createElement('div');
 mhNewPointer.id = 'mouse';
@@ -15,8 +17,8 @@ document.getElementsByTagName('body')[0].appendChild(mhNewPointer);
 
 /* Move the div to the mouse */
 function mhHandleMouseMove(event) {
-    newX = event.pageX - 25;
-    newY = event.pageY - 25;
+    newX = event.pageX - circleRadius;
+    newY = event.pageY - circleRadius;
     console.log("X: "+newX+" Y:"+newX)
     mhNewPointer.style.left = newX + 'px';
     mhNewPointer.style.top = newY + 'px';
@@ -26,11 +28,11 @@ function mhHandleMouseMove(event) {
 function mhStyleMouseHighlight() {
     mhNewPointer.style.position = 'absolute';
     mhNewPointer.style.backgroundColor = '#000000';
-    mhNewPointer.style.width = '50px';
-    mhNewPointer.style.height = '50px';
+    mhNewPointer.style.width = `${circleDiameter}px`;
+    mhNewPointer.style.height = `${circleDiameter}px`;
     mhNewPointer.style.opacity = 0.25;
-    mhNewPointer.style.borderRadius = '100px';
+    mhNewPointer.style.borderRadius = `${circleDiameter}px`;
     mhNewPointer.style.borderColor = "#000000"
-    mhNewPointer.style.zIndex = '2147483647';
+    mhNewPointer.style.zIndex = '-1';
     mhNewPointer.style.pointerEvents = 'none';
 }
