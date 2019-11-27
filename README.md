e443bf65898d4639e9acb3d0ca3556a362545a5a
RohanGautam <rohan1gautam@gmail.com>
2019-11-27 22:01:11 +0800

    added radio buttons

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ Might be used with mouse-to-joystick concept or standalone.
 * [Add color dynamically to added text](https://stackoverflow.com/questions/22672131/add-color-dynamically-to-added-text)
 * <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 * [Keypress example](https://gist.github.com/SathyaBhat/894012) and [keycode reference](https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode)
+* [Close popup when button in it clicked](https://stackoverflow.com/questions/13207129/close-a-chrome-extension-popup-by-clicking-the-browser-action-icon-again)
+* [Getting current tab from popup](https://stackoverflow.com/questions/13359421/chrome-extension-get-current-tab-from-popup)
+* [Call background from popup](https://stackoverflow.com/questions/21146457/chrome-extension-getbackgroundpage-function-example)
--- popup.js
+++ popup.js
@@ -1,11 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     var checkPageButton = document.getElementById('clickIt');
-    checkPageButton.addEventListener('click', function () {
-        var background = chrome.extension.getBackgroundPage();
-        var query = { active: true, currentWindow: true };
-        chrome.tabs.query(query, function(tabs){
-            background.main(tabs[0]);
+    $(document).ready(function () {
+        $('input:radio[name=selectionMechanism]').change(function () {
+            if (this.value == 'keyboard') {
+                keyboardUseHandler()
+            }
+            else if (this.value == 'face') {
+                alert("face pressed");
+            }
         });
-        window.close(); // to close the popup on click of this button - dont do it if using popup window for something.
-    }, false);
+    });
+    // checkPageButton.addEventListener('click', function () {        
+    // }, false);
 }, false);
+
+
+function keyboardUseHandler() {
+    var background = chrome.extension.getBackgroundPage();
+    var query = { active: true, currentWindow: true };
+    chrome.tabs.query(query, function (tabs) {
+        background.main(tabs[0]);
+    });
+    window.close();// to close the popup on click of this button - dont do it if using popup window for something.
+}
