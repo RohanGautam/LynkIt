57c1f188fc50d18223fcc4613ba520bbed484d8c
RohanGautam <rohan1gautam@gmail.com>
2019-12-02 22:06:38 +0800

    working in extension!

--- background.js
+++ background.js
@@ -1,18 +1,23 @@
 chrome.browserAction.onClicked.addListener();
 
 function main(tab) {
-    chrome.tabs.executeScript(null, { file: "external/jquery-3.4.1.min.js" }, function() {
-        chrome.tabs.executeScript(tab.id, {file: "highlight.js"});
+
+  chrome.tabs.executeScript(null, { file: "external/jquery-3.4.1.min.js" }, function () {
+    chrome.tabs.executeScript(null, { file: "external/clmtrackr.js" }, function () {
+      chrome.tabs.executeScript(null, { file: "external/utils.js" }, function () {
+        chrome.tabs.executeScript(tab.id, { file: "highlight.js" });
+      });
     });
+  });
 }
 
 // Do first-time setup to gain access to webcam, if necessary.
 chrome.runtime.onInstalled.addListener((details) => {
-    if (details.reason.search(/install/g) === -1) {
-      return;
-    }
-    chrome.tabs.create({
-      url: chrome.extension.getURL('welcome.html'),
-      active: true
-    });
+  if (details.reason.search(/install/g) === -1) {
+    return;
+  }
+  chrome.tabs.create({
+    url: chrome.extension.getURL('welcome.html'),
+    active: true
   });
+});
