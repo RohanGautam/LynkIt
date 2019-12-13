e4c412cc0e67372d4d8e92988424150e556a5133
RohanGautam <rohan1gautam@gmail.com>
2019-12-13 16:46:54 +0800

    reordering path changes

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.browserAction.onClicked.addListener();
+
+function main(tab) {
+  chrome.tabs.executeScript(null, { file: "external/jquery-3.4.1.min.js" }, function () {
+        chrome.tabs.executeScript(tab.id, { file: "js/highlight.js" });
+      });
+}
