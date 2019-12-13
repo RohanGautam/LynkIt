97bc933ac05a88c331468c6b5477114d9bf3f032
RohanGautam <rohan1gautam@gmail.com>
2019-12-13 16:34:10 +0800

    folder reorganisation

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
 chrome.browserAction.onClicked.addListener();
 
 function main(tab) {
-  chrome.tabs.executeScript(null, { file: "external/jquery-3.4.1.min.js" }, function () {
+  chrome.tabs.executeScript(null, { file: "../external/jquery-3.4.1.min.js" }, function () {
         chrome.tabs.executeScript(tab.id, { file: "highlight.js" });
       });
 }
